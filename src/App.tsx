293d4854a2976454445ae68f7d5b32475ec257b6
Khalid-Parvaiz <khalidparvaiz917@gmail.com>
2026-03-30 10:06:07 +0500

    page problem solve

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <>
       <h2>Welcome To My App</h2>
-      <a href="/react-practice/counter">Counter</a>
-      <a href="/react-practice/counter">ToDo List</a>
+      <a href="/react-practice/counter/">Counter</a>
+      <a href="/react-practice/todo-list/">ToDo List</a>
 
     </>
   )
